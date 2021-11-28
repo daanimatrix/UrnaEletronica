6c25e2a105566eb26ecae6e8e42d8bb1aa2fb7b5
daanimatrix <52839563+daanimatrix@users.noreply.github.com>
2021-11-28 17:00:27 -0300

    tendo a tela de finalização

--- urna/script.js
+++ urna/script.js
@@ -8,6 +8,7 @@ let numeros = document.querySelector('.d-1-3');
 let etapaAtual = 0;
 let numero = '';
 let VotoBranco = false;
+let votos = [];
 
 function comecarEtapa(){
 
@@ -107,10 +108,17 @@ function confirma(){
     let votoConfirmado = false;
     if(VotoBranco === true){
         votoConfirmado = true;
-        console.log("confirmando como BRANCO")
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: 'branco'
+        });
+
     } else if(numero.length === etapa.numeros){
         votoConfirmado = true;
-        console.log("confirmando como "+ numero);
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        });
     }
 
     if(votoConfirmado){
@@ -119,7 +127,8 @@ function confirma(){
         if(etapas[etapaAtual] !== undefined){
             comecarEtapa();
         }else{
-            console.log("FIM!");
+            document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca">FIM</div>'
+            console.log(votos);    
         }
     }
 }
